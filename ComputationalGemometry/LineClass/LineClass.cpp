a6862866a9c2c0ca2a00735df19e0b630a417d83
jcpwfloi <jcpwfloi@gmail.com>
2014-05-10 21:36:19 +0800

    add LineClass

---
+++ ComputationalGemometry/LineClass/LineClass.cpp
@@ -0,0 +1,72 @@
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#define gi gI()
+
+using namespace std;
+
+const double eps = 1e-6;
+
+typedef long long ll;
+
+inline ll gI() {
+    long long flag = 1, p = 0;
+    char c = getchar();
+    while ((c < '0' || c > '9') && c != '-') c = getchar();
+    if (c == '-') flag = -1, c = getchar();
+    while ('0' <= c && c <= '9') p = p * 10 + (c - '0'), c = getchar();
+    return p * flag;
+}
+
+struct Point {
+    double x, y;
+    Point(double x, double y) : x(x), y(y) {}
+    Point() : x(0), y(0) {}
+    Point operator + (Point x) {
+	return Point(this -> x + x.x, this -> y + x.y);
+    }
+    Point operator - (Point x) {
+	return Point(this -> x - x.x, this -> y - x.y);
+    }
+    double operator ^ (Point x) {
+	return this -> x * x.x + this -> y * x.y;
+    }
+    double operator * (Point x) {
+	return this -> x * x.y - this -> y * x.x;
+    }
+};
+
+struct Line {
+    Point a, b;
+    Line(double x1, double y1, double x2, double y2) : a(Point(x1, y1)), b(Point(x2, y2)) {}
+    Line(Point a, Point b) : a(a), b(b) {}
+    Line() : a(Point(0, 0)), b(Point(0, 0)) {}
+};
+
+typedef Point Vector;
+
+inline bool isZero(double x) {if (fabs(x) <= eps) return true; else return false;}
+inline int sgn(double x) {if (isZero(x)) return 0;if (x > 0) return 1; else return -1;}
+Vector Vectorize(Point a, Point b) {
+    return b - a;
+}
+
+bool isIntersect(Line a, Line b) {
+    int judge1 = sgn((Vectorize(a.a, b.a) * Vectorize(a.a, b.b)) * (Vectorize(a.b, b.a) * Vectorize(a.b, b.b)));
+    int judge2 = sgn((Vectorize(b.a, a.a) * Vectorize(b.a, a.b)) * (Vectorize(b.b, a.a) * Vectorize(b.b, a.b)));
+    if (judge1 == -1 && judge2 == -1) return true;
+    return false;
+}
+
+int main() {
+    while (1) {
+	puts("Input Line:");
+	int judge = isIntersect(Line(gi, gi, gi, gi), Line(gi, gi, gi, gi)) ;
+	if (judge)
+	    puts("Intersect");
+	else
+	    puts("Not Intersect");
+    }
+    return 0;
+}
+
